e90a9bf14b9899ce92115e59fd3fe9aca53c736d
rails-driver <driverrails@gmail.com>
2016-10-13 17:25:15 +0300

    add spec for user model

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
 FactoryGirl.define do
   factory :account do
+    user
+    seller_id Faker::Number.number(10)
+    mws_auth_token Faker::Number.number(10)
+
+    association :marketplace
   end
 end
--- spec/factories/fulfillment_inbound_shipment.rb
+++ spec/factories/fulfillment_inbound_shipment.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 include ActionDispatch::TestProcess
 
 FactoryGirl.define do
