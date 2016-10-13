ec6342c79534c08839bea874989c945a64cac050
rails-driver <driverrails@gmail.com>
2016-10-13 15:19:46 +0300

    add processing minimal received_date for shipment and show this value

--- lib/data_processing/data_processor.rb
+++ lib/data_processing/data_processor.rb
@@ -19,24 +19,35 @@ module DataProcessing
         shipments_without_price = ReceivedInventory.where(price_total: nil).distinct.pluck(:fba_shipment_id)
         shipment_ids_all_items_priced = received_inventories_priced.where.not(fba_shipment_id: shipments_without_price).distinct.pluck(:fba_shipment_id)
         shipment_ids_all_items_priced.each do |item|
-          request = ActiveRecord::Base.connection.execute("SELECT SUM(quantity) AS total_quantity, SUM(price_total) AS total_price FROM received_inventories WHERE fba_shipment_id = '#{item}'")
+          request = ActiveRecord::Base.connection.execute("SELECT
+                                                             SUM(quantity) AS total_quantity,
+                                                             SUM(price_total) AS total_price,
+                                                             MIN(received_date) AS date
+                                                           FROM received_inventories WHERE fba_shipment_id = '#{item}'")
           shipment_items_quantity = request.field_values('total_quantity').first
           shipment_total_cost = request.field_values('total_price').first
-          FulfillmentInboundShipment.create(fulfillment_inbound_shipment_params(shipment_total_cost, shipment_items_quantity, item, current_user))
+          shipment_minimal_date = request.field_values('date').first
+          shipment_details = { shipment: item,
+                               quantity: shipment_items_quantity,
+                               cost: shipment_total_cost,
+                               date: shipment_minimal_date }
+          FulfillmentInboundShipment.create(fulfillment_inbound_shipment_params(shipment_details, current_user))
         end
       end
 
       private
 
-      def fulfillment_inbound_shipment_params(cost, units, shipment_id, current_user)
+      def fulfillment_inbound_shipment_params(shipment_details, current_user)
+        shipment_id = shipment_details[:shipment]
         marketplace_id = ReceivedInventory.where(fba_shipment_id: shipment_id, marketplace: current_user.marketplaces).distinct.pluck(:marketplace_id).first
         marketplace = Marketplace.find(marketplace_id)
 
         {
           marketplace: marketplace,
           shipment_id: shipment_id,
-          price: cost,
-          total_received_units: units
+          price: shipment_details[:cost],
+          total_received_units: shipment_details[:quantity],
+          external_date_created: shipment_details[:date]
         }
       end
     end
