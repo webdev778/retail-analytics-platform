54e35a289608606ba80999aeb340d3f662939d51
rails-driver <driverrails@gmail.com>
2016-10-21 18:39:32 +0300

    fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,8 +7,4 @@ class DashboardController < ApplicationController
     @roi_and_sell_through_chart = TotalChartsService.new.roi_and_sell_through
     @total_data = ReceivedInventory.select('SUM(cost_remain) cost_remain').order(nil).first
   end
-
-  def total
-    @roi_chart = TotalChartsService.new.sales_and_inventory_turnover
-  end
 end
