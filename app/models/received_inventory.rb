b4bf5c37d40a2fb406471ab199201a5990557d73
rails-driver <driverrails@gmail.com>
2016-10-26 17:21:26 +0300

    add specs for situations

--- app/models/received_inventory.rb
+++ app/models/received_inventory.rb
@@ -2,7 +2,6 @@ class ReceivedInventory < ApplicationRecord
   belongs_to :marketplace
 
   scope :positive_quantity, -> { where('quantity > 0') }
-  # scope :sold, -> { where('quantity = sold_units') }
   scope :sold, -> { where('quantity = sold_units') }
   scope :with_unsold, -> { where('quantity > sold_units AND quantity > 0') }
 end
--- spec/lib/data_processing/data_processor_spec.rb
+++ spec/lib/data_processing/data_processor_spec.rb
@@ -188,4 +188,122 @@ describe DataProcessing::DataProcessor do
       expect(transaction.unprocessed_quantity).to eq 1
     end
   end
+
+  context 'transaction 5 received inventory list long' do
+    let(:transaction) do
+      create(:transaction,
+             marketplace: marketplace,
+             report: report,
+             date_time: 1.day.ago,
+             transaction_type: 'Order',
+             quantity: 5,
+             sku: '123')
+    end
+    let(:received_inventory_3) do
+      create(:received_inventory,
+             marketplace: marketplace,
+             quantity: 4,
+             sold_units: 1,
+             remain_units: 3,
+             product_name: 'third',
+             received_date: 2.days.ago,
+             sku: '123')
+    end
+
+    before do
+      account
+      transaction
+      # 5
+      received_inventory
+      # quantity: 1,
+      received_inventory_2
+      # 4
+      received_inventory_3
+      # 3
+    end
+
+    subject { DataProcessing::DataProcessor.transaction_processing(report) }
+
+    it 'should process report' do
+      subject
+      received_inventory.reload
+      received_inventory_2.reload
+      received_inventory_3.reload
+      report.reload
+      transaction.reload
+
+      expect(received_inventory.sold_units).to eq 1
+      expect(received_inventory_2.sold_units).to eq 4
+      expect(received_inventory_3.sold_units).to eq 1
+      expect(received_inventory_3.remain_units).to eq 3
+      expect(report.processed).to_not be_nil
+      expect(transaction.unprocessed_quantity).to eq nil
+    end
+  end
+
+  context 'transaction 5 received inventory list long and first enough' do
+    let(:transaction) do
+      create(:transaction,
+             marketplace: marketplace,
+             report: report,
+             date_time: 1.day.ago,
+             transaction_type: 'Order',
+             quantity: 5,
+             sku: '123')
+    end
+
+    let(:received_inventory) do
+      create(:received_inventory,
+             marketplace: marketplace,
+             quantity: 10,
+             remain_units: 8,
+             sold_units: 2,
+             product_name: 'first',
+             received_date: 4.days.ago,
+             sku: '123')
+    end
+
+    let(:received_inventory_3) do
+      create(:received_inventory,
+             marketplace: marketplace,
+             quantity: 4,
+             sold_units: 1,
+             remain_units: 3,
+             product_name: 'third',
+             received_date: 2.days.ago,
+             sku: '123')
+    end
+
+    before do
+      account
+      transaction
+      # 5
+      received_inventory
+      # quantity: 8,
+      received_inventory_2
+      # 4
+      received_inventory_3
+      # 3
+    end
+
+    subject { DataProcessing::DataProcessor.transaction_processing(report) }
+
+    it 'should process report' do
+      subject
+      received_inventory.reload
+      received_inventory_2.reload
+      received_inventory_3.reload
+      report.reload
+      transaction.reload
+
+      expect(received_inventory.sold_units).to eq 7
+      expect(received_inventory.remain_units).to eq 3
+      expect(received_inventory_2.remain_units).to eq 4
+      expect(received_inventory_2.sold_units).to eq 0
+      expect(received_inventory_3.sold_units).to eq 1
+      expect(received_inventory_3.remain_units).to eq 3
+      expect(report.processed).to_not be_nil
+      expect(transaction.unprocessed_quantity).to eq nil
+    end
+  end
 end
