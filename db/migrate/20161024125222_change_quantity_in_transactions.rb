5b8ad20e465bd87ca8b0be63b9ed3d46f8e6748f
rails-driver <driverrails@gmail.com>
2016-10-27 13:42:38 +0300

    remove unneeded migration

--- db/migrate/20161024125222_change_quantity_in_transactions.rb
+++
@@ -1,5 +0,0 @@
-class ChangeQuantityInTransactions < ActiveRecord::Migration[5.0]
-  def change
-    change_column :transactions, :quantity, :decimal, precision: 10, scale: 2, using: 'CAST(total AS integer)'
-  end
-end
